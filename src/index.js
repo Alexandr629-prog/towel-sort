9e20c4ceaffc56939164014294480116926c8f81
Alexandr629-prog <aaskaldovic@gmail.com>
2020-03-22 14:28:19 +0300

    redid the task

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-
-
-  return Array.prototype.concat.apply([], matrix);;
+var newarr = [];
+var k = 0;
+for (var i = 0; i < matrix.length; i++)
+for (var j = 0; j <matrix[i].length; j++)
+{
+newarr[k] = arr[i][j];
+k++;
+}
+return newarr;
 }
