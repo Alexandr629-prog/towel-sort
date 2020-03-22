922778ec0febc65406b7f37d856bbbd10a50d8ec
Alexandr629-prog <aaskaldovic@gmail.com>
2020-03-22 17:07:13 +0300

    redid the task

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@
 module.exports = function towelSort (matrix) {
 var newarr = [];
 var k = 0;
-if(matrix==undefined) return [];
-if(matrix.length==1) return matrix;
+if(matrix===undefined) return [];
+if(matrix.length===1) return matrix;
 for (var i = 0; i < matrix.length; i++)
 {
-if( i==0 || i%2==0)
+if( i===0 || i%2===0)
 {
 for (var j = 0; j <matrix[i].length; j++)
 {
